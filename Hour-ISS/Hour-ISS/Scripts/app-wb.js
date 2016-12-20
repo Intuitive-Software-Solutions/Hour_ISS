fa8e3cb24a92f1d035da007417d5971a5737b4a4
ntirnanich <ntirnanich@yahoo.com>
2016-12-20 16:17:43 -0600

    worldbank api set...working on more utility

---
+++ Hour-ISS/Hour-ISS/Scripts/app-wb.js
@@ -0,0 +1,21 @@
+﻿var getWorldBankCountries = function () {
+    let worldbankUrl = "http://api.worldbank.org/countries?format=jsonp";
+    console.log(worldbankUrl);
+    $.ajax({
+        url: worldbankUrl,
+        jsonp: "prefix",
+        dataType: "JSONP",
+        success: function (data) {
+            console.log("worldbank", data);
+
+            return data;
+
+        }
+    }).done(function (data) {
+        console.log("worldbank done");
+    }
+).fail(function (data) {
+    console.log("!!!!! worldbank failed");
+    console.log(data);
+})
+}
