74ee3d555fe6310299da6469ae9e855a04a07704
Adam Bakken <bakken.adam@gmail.com>
2016-12-20 15:42:14 -0600

    The map of the earth now displays on page, markers however are dummy data

--- Hour-ISS/Hour-ISS/Scripts/app-map.js
+++ Hour-ISS/Hour-ISS/Scripts/app-map.js
@@ -10,15 +10,14 @@ function initMap() {
         mapTypeId: 'terrain'
     });
     var script = document.createElement('script');
-    script.src = '/app.js/getISSLocations';
+    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
     console.log("Are we there yet?");
-    console.log(script.src);
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 window.eqfeed_callback = function(results){
     for (var i = 0; i < results.features.length; i++){
         var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(lat[1], lon[2]);
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
         var marker = new google.maps.Marker({
             position: latLng,
             map: map
