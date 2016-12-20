dfc2ee7caff24cf58b9679fe0d69a87cb9ba218f
Adam Bakken <bakken.adam@gmail.com>
2016-12-20 16:15:06 -0600

    Dummy data removed from map markers, now working on implementing our own data.

--- Hour-ISS/Hour-ISS/Scripts/app-map.js
+++ Hour-ISS/Hour-ISS/Scripts/app-map.js
@@ -1,18 +1,23 @@
 ﻿$(document).ready(function () {
-    initMap();
+    let now = Date.now();
+    let hoursList = getPast24Hrs(now);
+    var newmap = getISSLocations(hoursList);
+    initMap(newmap);
 });
 
 var map;
-function initMap() {
+function initMap(array) {
         map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
         center: new google.maps.LatLng(0, 0),
         mapTypeId: 'terrain'
     });
     var script = document.createElement('script');
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    script.src = 'app.js';
     console.log("Are we there yet?");
+    console.log(array);
     document.getElementsByTagName('head')[0].appendChild(script);
+    
 }
 window.eqfeed_callback = function(results){
     for (var i = 0; i < results.features.length; i++){
